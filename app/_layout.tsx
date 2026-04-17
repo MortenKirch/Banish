a55fad7f6244ac119d2e8e7b16c885d55e754942
MortenKirch <morten-kris@live.dk>
2026-04-17 10:07:03 +0200

    Add: authentication to Rootlayout to handle auth routes sign up and signin

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,19 +3,31 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "./globals.css";
 
+import { useAuthContext } from "@/hooks/use-auth-context";
 import { useColorScheme } from "@/hooks/use-color-scheme";
+import AuthProvider from "@/providers/auth-provider";
+import { ActivityIndicator, View } from "react-native";
 
 export const unstable_settings = {
   anchor: "(tabs)",
 };
 
-export default function RootLayout() {
+function RootNavigator() {
+  const { isLoggedIn, isLoading } = useAuthContext();
   const colorScheme = useColorScheme();
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
@@ -27,7 +39,16 @@ export default function RootLayout() {
           options={{ presentation: "modal", title: "Modal" }}
         />
       </Stack>
+      {!isLoggedIn && <Redirect href="/(auth)/login" />}
       <StatusBar style="auto" />
     </ThemeProvider>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <AuthProvider>
+      <RootNavigator />
+    </AuthProvider>
   );
 }
